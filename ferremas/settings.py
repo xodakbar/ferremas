1b0ec1edb484cff0f5fed3b416b032db5cd49233
JoacoRC <jo.romeroc@duocuc.cl>
2025-06-03 16:14:14 -0400

    asi nomas

--- ferremas/settings.py
+++ ferremas/settings.py
@@ -83,16 +83,11 @@ WSGI_APPLICATION = 'ferremas.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:dYLRTgdKROhCoDSwkaQRZxCEYLAjEzYr@switchyard.proxy.rlwy.net:38201/railway',
-        conn_max_age=600,
-        ssl_require=True
-    )
-        
-        
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # o 'nombre_personalizado.sqlite3'
+    }
 }
 
 
