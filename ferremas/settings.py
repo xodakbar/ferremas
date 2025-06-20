b2f1a279d9201b156c416a208ff101200054ecc9
JoacoRC <135282758+JoaquinRCDC@users.noreply.github.com>
2025-06-19 23:50:42 -0400

    bbdd nube

--- ferremas/settings.py
+++ ferremas/settings.py
@@ -83,11 +83,24 @@ WSGI_APPLICATION = 'ferremas.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+import os
+from dotenv import load_dotenv
+
+
+# Busca y carga el archivo .env
+load_dotenv()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # o 'nombre_personalizado.sqlite3'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PGDATABASE'),
+        'USER': os.environ.get('PGUSER'),
+        'PASSWORD': os.environ.get('PGPASSWORD'),
+        'HOST': os.environ.get('PGHOST'),
+        'PORT': os.environ.get('PGPORT', '5432'),
     }
+
 }
 
 
